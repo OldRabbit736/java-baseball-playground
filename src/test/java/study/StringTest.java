ddb1c1b40f70a897bf0a25d49556f8ef26e3e8a5
OldRabbit736 <sylvan0212@gmail.com>
2022-04-03 19:19:57 +0900

    학습 테스트 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -10,4 +12,35 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split1() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void split2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String str = "(1,2)";
+        String substring = str.substring(1, str.length() - 1);
+
+        assertThat(substring).isEqualTo("1,2");
+
+    }
+
+    @Test
+    @DisplayName("인덱스를 넘어가면 예외가 발생한다.")
+    void charAt() {
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> "abc".charAt(3))
+                .withMessageContaining("String index out of range");
+    }
+
+
 }
